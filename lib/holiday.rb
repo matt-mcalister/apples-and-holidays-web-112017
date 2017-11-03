cf7039a7ea5cbb9bcb68d0d8f32dea12b89687a1
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-03 17:13:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -57,7 +57,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, data|
+    data.each do |holiday, supplies|
+        puts "#{season}:"
+        puts "  #{holiday}: #{supplies.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
